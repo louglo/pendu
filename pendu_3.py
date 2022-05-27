30b30ea6fa04176afdd537c9124162d6a9915879
louglo <91287515+louglo@users.noreply.github.com>
2022-05-27 23:01:24 +0200

    Add files via upload

---
+++ pendu_3.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python
+
+#fonction permettant de choisir élément randtestom d'une liste donnée
+import random
+
+banque = ["poulet","bidet","menthe","patate","cajou"]
+
+guess = random.choice(banque)
+
+mort = 0
+
+#join permet de display une liste sous forme de string
+progres = []
+for lettre in range(len(guess)):
+    progres.append("_")
+
+#outline = "_ " * len(guess)
+#print ("Essaie de deviner le mot : " + outline)
+print ("Essaie de deviner le mot : " + " ".join(progres))
+print ("Si tu n'as pas trouvé au bout du 8ème essai... COUIC..!")
+
+# test[0] au cas où des gens écrivent plusieurs lettres seule la première sera gardée
+# on ne peut pas faire str + int donc on convertit tour en string avec str(tour)
+while mort < 8:
+    nul = 0
+    
+    print ("")
+    if mort == 7:
+        print ("Attention dernière tentative")
+    else:
+       print ("Essai " + str(mort + 1))  
+       
+    test = input("Choisis une lettre: ")
+    print ("Tu as proposé: " + "'" + test[0] + "'")
+    print ("Vérification...")
+    
+    # si juste guess à la place range() erreur à la ligne suivante pour "guess[lettre]" demande int:
+    for lettre in range(len(guess)):
+    #si bon, remplace le "_" par la lettre devinée
+        if test[0] == guess[lettre]:
+            progres[lettre] = test[0]
+        else:
+            nul = nul + 1
+
+   #si nul < longueur du mot alors il y a eu des lettres découvertes à cette boucle
+    if nul < len(guess):
+        print ("Bien joué")
+    else:
+        mort = mort + 1
+        print ("Raté")
+    print (" ".join(progres))
+
+    if "_" not in progres:
+        print("Bravo tu as gagné!!!")
+        quit()
+
+print ("")
+print ("Perdu!!!")
+print ("Le mot était " + guess)
